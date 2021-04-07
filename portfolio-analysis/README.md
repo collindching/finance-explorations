731eb609c7251fcecd44576745ab9b4e932f9c40
Collin <collindching@gmail.com>
2021-04-06 22:14:50 -0700

    pull portfolio tickers, start pulling price data

--- portfolio-analysis/README.md
+++ portfolio-analysis/README.md
@@ -1,12 +1,25 @@
-Questions
+# Tasks
 
 1. Set up the TDAmeritrade account
 2. Set up authentication in R
 3. Analyze portfolio
 
+# Questions
 
-https://exploringfinance.github.io/posts/2020-01-01-exploring-finance/
+- How much money have I left on the table? 
+- How risky is my portfolio? 
+- How could I improve my portfolio?
 
-https://github.com/exploringfinance/rameritrade
-https://github.com/TDAmeritrade/stumpy
-https://github.com/jbertovic/tdacli
+# Key packages
+
+- `rameritrade`
+- `PerformanceAnalytics`
+- `quantmod`
+
+# Links
+
+- [rameritrade](https://github.com/exploringfinance/rameritrade)
+- [Documentation for rameritrade package](https://cran.r-project.org/web/packages/rameritrade/rameritrade.pdf)
+- https://exploringfinance.github.io/posts/2020-01-01-exploring-finance/
+- https://github.com/TDAmeritrade/stumpy
+- https://github.com/jbertovic/tdacli
--- portfolio-analysis/scripts/02_explore-account.R
+++ portfolio-analysis/scripts/02_explore-account.R
@@ -1,10 +1,41 @@
 library(rameritrade)
+library(lubridate)
+library(tidyverse)
+library(PerformanceAnalytics)
+library(quantmod)
 
 refreshToken = readRDS('TDRefToken.rds')
-consumerKey = readRDS('consumerKey.Rds')
+consumerKey = readRDS('consumerKey.rds')
+accountIDs = readRDS('accountIDs.rds')
 
 accessToken = rameritrade::td_auth_accessToken(consumerKey, refreshToken)
 
-df_act = td_accountData()
-df_act
-names(df_act$positions)
+act_dt = td_accountData()
+
+names(act_dt)
+names(act_dt$positions)
+
+df_pos <- act_dt$positions
+df_equity <- df_pos %>%
+    filter(!grepl('_',instrument.symbol)) %>%
+    select(account = accountId,
+           ticker = instrument.symbol,
+           shares= longQuantity,
+           avg_price = averagePrice,
+           market_value = marketValue) %>%
+    mutate(weight = market_value/sum(market_value)) %>%
+    arrange(desc(weight))
+
+
+symbols <- df_equity$ticker
+pr_daily <- getSymbols(symbols, src = "yahoo",
+                       from = "2015-01-01",
+                       to = "2021-03-05",
+                       auto.assign = T) 
+
+get(pr_daily[[1]])
+
+View(df_equity)
+
+
+    
